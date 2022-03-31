70515d8c7ff330196bbdca640545a2e34b298608
DIEGO SANTOS DOS SANTOS <84682022+DS-DIEGOSANTOS@users.noreply.github.com>
2022-03-31 18:12:56 -0300

    aa

--- exercicio 04/src/aplicacao/principal.java
+++ exercicio 04/src/aplicacao/principal.java
@@ -1,5 +1,23 @@
 package aplicacao;
 
+import java.util.Scanner;
+
 public class principal {
+	public static void main(String[] args) {
+		int idade;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite sua idade:");
+		idade = entrada.nextInt();
+		
+		if(idade>18 || idade < 67) {
+			System.out.println("pode doar sangue");
+		}else {
+			System.out.println("não pode doar sangue");
+		}
+
+		entrada.close();
+	}
 
 }
